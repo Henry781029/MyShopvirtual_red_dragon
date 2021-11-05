794878c15c929bcb5431a5d37a9add755c7a6631
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-11-05 16:27:44 -0500

    se completa el registro en bd firebase y crea acivity view udser y seller

---
+++ app/src/main/java/com/project_moviles2/miShopVirtual/View_UserSeller.java
@@ -0,0 +1,14 @@
+package com.project_moviles2.miShopVirtual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_UserSeller extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_user_seller);
+    }
+}
--- app/src/main/java/com/project_moviles2/miShopVirtual/shop_register.java
+++ app/src/main/java/com/project_moviles2/miShopVirtual/shop_register.java
@@ -1,9 +1,12 @@
 package com.project_moviles2.miShopVirtual;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -13,8 +16,20 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Document;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class shop_register extends AppCompatActivity {
 
@@ -25,6 +40,8 @@ public class shop_register extends AppCompatActivity {
     ImageView jivimagen2;
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,15 +73,14 @@ public class shop_register extends AppCompatActivity {
 
     }
     public void Registrar (View view){
-        String name, emailregister,country, city, password2, role, shop;
+        final String name, emailregister,country, city, password2, role, shop;
 
         name=jetname.getText().toString();
         emailregister=jetemailregister.getText().toString();
         country=jetcountry.getText().toString();
         city=jetcity.getText().toString();
         password2=jetpassword2.getText().toString();
-        role=jetrole.getText().toString();
-        shop=jetrole.getText().toString();
+        shop=jetshop.getText().toString();
 
         if (name.isEmpty() ){
 
@@ -92,10 +108,10 @@ public class shop_register extends AppCompatActivity {
             jetpassword2.requestFocus();
 
         }
-        else if (role.isEmpty()){
+        else if (password2.length()<6){
 
-            Toast.makeText(getApplicationContext(),"Se debe ingresar role",Toast.LENGTH_LONG).show();
-            jetrole.requestFocus();
+            Toast.makeText(getApplicationContext(),"La contraseña debe contener  6 o más caracteres",Toast.LENGTH_LONG).show();
+            jetpassword2.requestFocus();
 
         }
         else if (shop.isEmpty()){
@@ -104,11 +120,108 @@ public class shop_register extends AppCompatActivity {
             jetshop.requestFocus();
 
         }else{
-            Toast.makeText(getApplicationContext(), "El Registro se ha realizado correctamente", Toast.LENGTH_SHORT).show();
+
+            DocumentReference docRef= db.collection("users").document(emailregister);
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if(task.isSuccessful()){
+                        DocumentSnapshot document =task.getResult();
+                        if(document.exists()){
+                            AlertDialog.Builder builder = new AlertDialog.Builder(shop_register.this);
+                            builder.setTitle("Usuario ya existe!!");
+                            builder.setMessage("Deseas iniciar sesion con:\n "+emailregister+"?")
+                                    .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                            intent.putExtra("coleccion", emailregister);
+                                            startActivity(intent);
+                                        }
+                                    })
+                                    .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Toast.makeText(getApplicationContext(), "Por favor crear cuenta con otro nombre de usuario", Toast.LENGTH_LONG).show();
+                                            dialog.dismiss();
+                                        }
+                                    }).show();
+
+
+                        }
+                        else{
+                            Map<String, Object> user = new HashMap<>();
+
+                            user.put("Password", password2);
+
+                            if(jrbseller.isChecked()){
+                                user.put("Rol","Seller");
+                            }else if (jrbuser.isChecked()){
+                                user.put("Rol", "user");
+                            }
+
+                            user.put("Name", name);
+                            user.put("Country", country);
+                            user.put("City", city);
+
+                            user.put("Name", name);
+                            user.put("Country", country);
+                            user.put("City", city);
+                            user.put("Shop", shop);
+
+                            db.collection("users").document(emailregister)
+                                    .set(user)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Log.d("Registro ok", "DocumentSnapshot successfully written!");
+                                            Toast.makeText(getApplicationContext(), "El Registro de Usuario se ha realizado correctamente", Toast.LENGTH_SHORT).show();
+                                            Intent intent = new Intent(getApplicationContext(),View_UserSeller.class);
+
+                                            intent.putExtra("coleccion", emailregister);
+                                            startActivity(intent);
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w("Registro ok", "Error writing document",e);
+                                        }
+                                    });
+
+                        }
+
+
+                    }
+
+
+                }
+
+            });
+
         }
 
     }
 
+    public void Autenticar (){
+
+        String email= jetemailregister.getText().toString();
+        String password=jetpassword2.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Usuario Registrado Correctamente", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(shop_register.this,"no funciona esta mierda",Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
 
     public void Back (View view){
 
